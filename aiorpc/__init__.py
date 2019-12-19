5890bc54eca51b7027526b13cf16514244b4af29
Aratz M. Lasa <aratzml@opendeusto.es>
2019-12-19 01:28:14 +0100

    Server to class
    Backwards compatibility
    
    Backwards compatibility
    
    Backwards compatibility

--- aiorpc/__init__.py
+++ aiorpc/__init__.py
@@ -1,4 +1,4 @@
 from aiorpc.client import RPCClient
 from aiorpc.server import *
 
-__all__ = ['RPCClient', 'register', 'msgpack_init', 'set_timeout', 'serve']
+__all__ = ['RPCClient', 'RPCServer', 'register', 'msgpack_init', 'set_timeout', 'serve', 'register_class']
