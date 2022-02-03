20f11cf34cdcb1f952f5e975cce60912d3c809aa
Tomas Farago <sensej007@email.cz>
2022-02-03 13:56:13 +0100

    examples: add dictionary with non-string keys

--- examples/client.py
+++ examples/client.py
@@ -6,9 +6,11 @@ import uvloop
 async def do(cli):
     ret = await client.call('echo', 'message')
     print("{}\n".format(ret))
+    ret = await client.call('get_dict')
+    print("{}\n".format(ret))
 
 loop = uvloop.new_event_loop()
 asyncio.set_event_loop(loop)
-client = RPCClient('127.0.0.1', 6000)
+client = RPCClient('127.0.0.1', 6000, unpack_params={'strict_map_key': False})
 loop.run_until_complete(do(client))
 client.close()
--- examples/server.py
+++ examples/server.py
@@ -7,9 +7,13 @@ import uvloop
 def echo(msg):
     return msg
 
+def get_dict():
+    return {1: 2}
+
 loop = uvloop.new_event_loop()
 asyncio.set_event_loop(loop)
 register("echo", echo)
+register("get_dict", get_dict)
 coro = asyncio.start_server(serve, '127.0.0.1', 6000, loop=loop)
 server = loop.run_until_complete(coro)
 
